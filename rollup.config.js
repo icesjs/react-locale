08eb312c37a4481df7961adb0c24cbda358eed76
Stone <star2018@outlook.com>
2021-01-10 09:44:54 +0800

    build: optimize rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,36 +2,43 @@ import typescript from '@rollup/plugin-typescript'
 import externals from 'rollup-plugin-node-externals'
 import pkg from './package.json'
 
-function getCommonsPlugin() {
+const isEnvDevelopment = process.env.NODE_ENV === 'development'
+
+function getPlugins(format) {
   return [
     externals({
       builtins: true,
       deps: true,
       peerDeps: true,
     }),
+    typescript({
+      removeComments: !isEnvDevelopment,
+      inlineSources: !isEnvDevelopment,
+      noUnusedLocals: !isEnvDevelopment,
+      target: format === 'es' ? 'esnext' : 'es5',
+    }),
   ]
 }
 
+const input = 'src/index.ts'
+
 export default [
   {
-    input: 'src/index.ts',
-    output: [{ file: pkg.module, format: 'es', sourcemap: true, exports: 'auto' }],
-    plugins: [
-      typescript({
-        removeComments: true,
-      }),
-      ...getCommonsPlugin(),
-    ],
+    input,
+    output: {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: getPlugins('es'),
   },
   {
-    input: 'src/index.ts',
-    output: { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'auto' },
-    plugins: [
-      typescript({
-        removeComments: true,
-        target: 'es5',
-      }),
-      ...getCommonsPlugin(),
-    ],
+    input,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: getPlugins('cjs'),
   },
 ]
