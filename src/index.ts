7b47c746fd63db4544f572f1c173ef4bdcb86381
Stone <star2018@outlook.com>
2021-01-11 20:58:57 +0800

    chore: update to fix d.ts declare

--- src/index.ts
+++ src/index.ts
@@ -28,3 +28,26 @@ export {
   MessageValue,
   MessageDefinitions,
 } from './message'
+
+// 下面定义的常量，仅为生成类型定义文件。
+// 如果需要直接使用这些常量，请用 withXxx 方法进行语言内容绑定，并获取相应方法。
+
+/**
+ * 类型组件内使用的转换组件。
+ */
+export const Translate: Translate = withDefinitionsComponent({})
+
+/**
+ * 类型组件内使用的转换组件，是Translate组件的别名。
+ */
+export const Trans: Translate = Translate
+
+/**
+ * 函数组件内使用的hook，可提供区域语言内容转换。
+ */
+export const useLocale: useLocale = withDefinitionsHook({})
+
+/**
+ * 函数组件内使用的hook，可提供区域语言内容转换。
+ */
+export default useLocale
