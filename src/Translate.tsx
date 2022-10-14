0636c7202da051f34e9fd9dfc9a8cf5978a1315c
icesjs <icesjs@outlook.com>
2022-10-14 11:27:28 +0800

    update.

--- src/Translate.tsx
+++ src/Translate.tsx
@@ -17,6 +17,8 @@ interface RendererVFCProps<T extends HTMLTagName = HTMLTagName>
   enableHTML?: boolean
   // 输出HTML内容时，包裹内容的标签名称。默认为span。
   tagName?: T
+  // 转发的元素引用
+  forwardedRef?: React.Ref<any>
 }
 
 interface LocaleTransProps extends Omit<RendererVFCProps, 'content'> {
@@ -31,12 +33,6 @@ interface LocaleTransProps extends Omit<RendererVFCProps, 'content'> {
   // 消息内容定义对象
   definitions?: MessageDefinitions | LocaleResourceLoader
 }
-
-type TransVFC = React.VoidFunctionComponent<LocaleTransProps>
-
-type ContextTransVFC = React.VoidFunctionComponent<
-  { contextType: React.Context<string> } & LocaleTransProps
->
 
 /**
  * 语言内容转译组件属性定义类型。
@@ -56,17 +52,24 @@ function isTHMLText(text: string) {
   return !/^<key>.*<\/key>$/.test(text) && /<.*?\/?>/.test(text)
 }
 
-function RenderTextOrHTML({ tagName, content, enableHTML, ...props }: RendererVFCProps) {
+function RenderTextOrHTML({
+  tagName,
+  content,
+  enableHTML,
+  forwardedRef,
+  ...props
+}: RendererVFCProps) {
   if (enableHTML && isTHMLText(content)) {
     return React.createElement(tagName || 'span', {
       ...props,
+      ref: forwardedRef,
       dangerouslySetInnerHTML: {
         __html: content,
       },
     })
   }
   if (tagName) {
-    return React.createElement(tagName, props, content)
+    return React.createElement(tagName, { ...props, ref: forwardedRef }, content)
   }
   return content
 }
@@ -74,39 +77,24 @@ function RenderTextOrHTML({ tagName, content, enableHTML, ...props }: RendererVF
 /**
  * 语言内容转译组件。
  */
-const LocaleTrans: TransVFC = ({
-  id,
-  fallback,
-  plugins,
-  data,
-  definitions,
-  enableHTML,
-  tagName,
-}) => {
+function LocaleTrans(props: LocaleTransProps) {
+  const { id, fallback, plugins, data, definitions, ...rest } = props
   const [translate] = useLocaleTrans(plugins, fallback, definitions)
   return (
     // @ts-ignore
-    <RenderTextOrHTML content={translate(id, data)} enableHTML={enableHTML} tagName={tagName} />
+    <RenderTextOrHTML {...rest} content={translate(id, data)} />
   )
 }
 
 /**
  * 绑定了上下文的语言内容转译组件。
  */
-const ContextLocaleTrans: ContextTransVFC = ({
-  id,
-  fallback,
-  plugins,
-  data,
-  contextType,
-  definitions,
-  enableHTML,
-  tagName,
-}) => {
+function ContextLocaleTrans(props: LocaleTransProps & { contextType: React.Context<string> }) {
+  const { id, fallback, plugins, data, definitions, contextType, ...rest } = props
   const [translate] = useContextLocaleTrans(contextType, plugins, fallback, definitions)
   return (
     // @ts-ignore
-    <RenderTextOrHTML content={translate(id, data)} enableHTML={enableHTML} tagName={tagName} />
+    <RenderTextOrHTML {...rest} content={translate(id, data)} />
   )
 }
 
@@ -124,14 +112,7 @@ export function withDefinitionsComponent(definitions?: MessageDefinitions | Loca
     render() {
       // 如果使用了上下文绑定，则使用绑定上下文的转译组件
       const { contextType, enableDangerouslySetInnerHTML: globalEnableHTML } = TranslateComponent
-      // 因为不能保证所有使用者都开启了ts校验，这里仍然检查下是否包含子组件
-      const { children, enableHTML = globalEnableHTML, ...rest } = this.props as any
-      if (children && !(typeof children === 'string' && !children.trim())) {
-        // 存在非空字符串的子组件时，则抛异常，因为子组件不会被渲染
-        throw new Error(
-          'The <Translate> component must be an empty component, but got some children within it.'
-        )
-      }
+      const { enableHTML = globalEnableHTML, ...rest } = this.props
       return contextType ? (
         <ContextLocaleTrans
           {...rest}
